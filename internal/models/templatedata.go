6339a4ad536c9b6c546799e02ad35f6ce65a4888
Shayne Meyer <shayne.meyer@gmail.com>
2024-05-09 08:14:25 -0700

    added support for server side validation

--- internal/models/templatedata.go
+++ internal/models/templatedata.go
@@ -1,5 +1,7 @@
 package models
 
+import "github.com/shaynemeyer/go-bnb/internal/forms"
+
 // TemplateData - holds data sent to handlers
 type TemplateData struct {
 	StringMap map[string]string
@@ -10,4 +12,5 @@ type TemplateData struct {
 	Flash     string
 	Warning   string
 	Error     string
+	Form      *forms.Form
 }
