f416fb1ad16e083d0da54e70db5624628c8ea568
Shayne Meyer <shayne.meyer@gmail.com>
2024-05-09 19:59:24 -0700

    added required fields and minLength validator

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -54,8 +54,13 @@ func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 
 // Reservation renders the make a reservation page and displays form
 func (m *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
+	var emptyReservation models.Reservation
+	data := make(map[string]interface{})
+	data["reservation"] = emptyReservation
+
 	render.RenderTemplate(w, r, "make-reservation.page.tmpl", &models.TemplateData{
 		Form: forms.New(nil),
+		Data: data,
 	})
 }
 
@@ -76,7 +81,8 @@ func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 
 	form := forms.New(r.PostForm)
 
-	form.Has("first_name", r)
+	form.Required("first_name", "last_name", "phone", "email")
+	form.MinLength("firs_name", 3, r)
 
 	if !form.Valid() {
 		data := make(map[string]interface{})
