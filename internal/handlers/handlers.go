ae5ccf97e602ce68593778de11cdc37ff810cd6c
Shayne Meyer <shayne.meyer@gmail.com>
2024-05-18 08:13:02 -0700

    added admin dropdown menu and flash when a user logs out

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -493,6 +493,8 @@ func (m *Repository) Logout(w http.ResponseWriter, r *http.Request) {
 	_ = m.App.Session.Destroy(r.Context())
 	_ = m.App.Session.RenewToken(r.Context())
 
+	m.App.Session.Put(r.Context(), "flash", "Logged out successfully")
+
 	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
 
